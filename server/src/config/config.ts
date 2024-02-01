e08baf133de0bdb1767cfa05957312eead34817d
molhamalrashed <molham.alrd@gmail.com>
2024-02-01 20:39:02 +0100

    server

---
+++ server/src/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyCOrgYQvjZ_5tmsQtMbKYPdbmNmTtUwsCY",
+        authDomain: "molham-typescript.firebaseapp.com",
+        projectId: "molham-typescript",
+        storageBucket: "molham-typescript.appspot.com",
+        messagingSenderId: "1073236978848",
+        appId: "1:1073236978848:web:0823b806844f3d11c9edc5"
+      },
+    server:{
+        url: 'http://localhost:3000'
+    }
+}
+
+export default config;
